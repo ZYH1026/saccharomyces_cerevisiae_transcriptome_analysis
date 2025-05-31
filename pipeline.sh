293392cce833afedbc21478789b7d8329f06ce4f
Zhao Yuehan <zhaoyuehan1026@gmail.com>
2025-05-31 13:04:46 +0800

    Update pipeline.sh

--- pipeline.sh
+++ pipeline.sh
@@ -1,158 +1,126 @@
 #!/bin/bash
-# This script is used to run the pipeline for the project.
-set -e  # Exit immediately if a command exits with a non-zero status.
+# RNA-seq variant calling pipeline v1.1
+set -e
 
-project_root="your_project_root_folder"
+# ====== 目录配置 ======
+project_root=$(pwd)
+data_dir="$project_root/data"
+raw_dir="$project_root/raw"
+report_dir="$project_root/report"
+vcf_dir="$project_root/vcf"
+ref_dir="$project_root/ref"
+
+mkdir -p {$data_dir,$raw_dir,$report_dir,$vcf_dir}
+
+# ====== 环境检查 ======
+if ! command -v hisat2 &> /dev/null; then
+    echo "错误：hisat2未安装！请先配置conda环境"
+    exit 1
+fi
+
+# ====== 数据下载 ======
+cd $raw_dir
+prefetch SRR5251643 SRR5251644
+fasterq-dump SRR5251643.sra
+fasterq-dump SRR5251644.sra
 cd $project_root
 
-# project_root (the folder of the project should look like this)
-#  |
-#  +-- data
-#  |
-#  +-- raw
-#  |
-#  +-- report
-#  |
-#  +-- vcf
-#  |
-#  +-- ref
-#  |  +-- Saccharomyces_cerevisiae.R64-1-1.dna.toplevel.fa
-#  |  +-- r64
-#  |     +-- genome.1.ht2
-#  |     +-- ......
+# ====== 质控 ======
+fastp -i $raw_dir/SRR5251643.fastq -o $data_dir/SRR5251643.clean.fastq.gz \
+      -h $report_dir/SRR5251643_fastp.html -j $report_dir/SRR5251643_fastp.json \
+      -q 20 -u 30
 
+fastp -i $raw_dir/SRR5251644.fastq -o $data_dir/SRR5251644.clean.fastq.gz \
+      -h $report_dir/SRR5251644_fastp.html -j $report_dir/SRR5251644_fastp.json \
+      -q 20 -u 30
 
-
-# Install softwares using conda
-conda create -n project_env
-conda activate project_env
-conda install -c bioconda fastp
-conda install -c bioconda hisat2
-conda install -c bioconda samtools
-conda install -c bioconda gatk4
-conda install -c bioconda sra-tools
-
-# Use prefetch to download the original sra files
-cd ./raw
-prefetch SRR5251643
-prefetch SRR5251644
-
-# Use fastq-dump to convert sra files to fastq files
-fasterq-dump SRR5251644.sra
-fasterq-dump SRR5251643.sra
-cd ..
-
-# Use fastp to trim the fastq files
-fastp -i ./raw/SRR5251644.fastq \
-      -o ./data/SRR5251644.clean.fastq.gz \
-      -h ./report/SRR5251644_fastp_report.html \
-      -j ./report/SRR5251644_fastp_report.json \
-      -q 20 \
-      -u 30
-
-fastp -i ./raw/SRR5251643.fastq \
-      -o ./data/SRR5251643.clean.fastq.gz \
-      -h ./report/SRR5251643_fastp_report.html \
-      -j ./report/SRR5251643_fastp_report.json \
-      -q 20 \
-      -u 30
-
-# Use hisat2 to align the clean fastq files to the reference genome
-hisat2 -p 8 \ 
--x ./ref/r64/genome \ 
--U ./data/SRR5251643.clean.fastq.gz \
--S ./data/SRR5251643_aligned.sam
+# ====== 比对 ======
+hisat2 -p 8 \
+       -x $ref_dir/r64/genome \
+       -U $data_dir/SRR5251643.clean.fastq.gz \
+       -S $data_dir/SRR5251643_aligned.sam
 
 hisat2 -p 8 \
--x ./ref/r64/genome \
--U ./data/SRR5251644.clean.fastq.gz \
--S ./data/SRR5251644_aligned.sam
+       -x $ref_dir/r64/genome \
+       -U $data_dir/SRR5251644.clean.fastq.gz \
+       -S $data_dir/SRR5251644_aligned.sam
 
-# Use samtools to convert the sam files to bam files, creating index files
-samtools view -b ./data/SRR5251644_aligned.sam | samtools sort -o ./data/SRR5251644.sorted.bam
-samtools index ./data/SRR5251644.sorted.bam
+# ====== SAM转BAM ======
+samtools view -b $data_dir/SRR5251643_aligned.sam | \
+samtools sort -o $data_dir/SRR5251643.sorted.bam
+samtools index $data_dir/SRR5251643.sorted.bam
 
-samtools view -b ./data/SRR5251643_aligned.sam | samtools sort -o ./data/SRR5251643.sorted.bam
-samtools index ./data/SRR5251643.sorted.bam
+samtools view -b $data_dir/SRR5251644_aligned.sam | \
+samtools sort -o $data_dir/SRR5251644.sorted.bam
+samtools index $data_dir/SRR5251644.sorted.bam
 
-# Use GATK to mark duplicates and index the bam files
+# ====== 标记重复 ======
 gatk MarkDuplicates \
-     -I ./data/SRR5251644.sorted.bam \
-     -O ./data/SRR5251644.dedup.bam \
-     -M ./report/SRR5251644.dedup.metrics.txt
-samtools index ./data/SRR5251644.dedup.bam
+     -I $data_dir/SRR5251643.sorted.bam \
+     -O $data_dir/SRR5251643.dedup.bam \
+     -M $report_dir/SRR5251643.dedup.metrics.txt
+samtools index $data_dir/SRR5251643.dedup.bam
 
 gatk MarkDuplicates \
-     -I ./data/SRR5251643.sorted.bam \
-     -O ./data/SRR5251643.dedup.bam \
-     -M ./report/SRR5251643.dedup.metrics.txt
-samtools index ./data/SRR5251643.dedup.bam
+     -I $data_dir/SRR5251644.sorted.bam \
+     -O $data_dir/SRR5251644.dedup.bam \
+     -M $report_dir/SRR5251644.dedup.metrics.txt
+samtools index $data_dir/SRR5251644.dedup.bam
 
-# Build the reference genome index using GATK and samtools
-gatk CreateSequenceDictionary \
-    -R ./ref/Saccharomyces_cerevisiae.R64-1-1.dna.toplevel.fa \
-    -O ./ref/Saccharomyces_cerevisiae.R64-1-1.dna.toplevel.dict
-
-samtools faidx ./ref/Saccharomyces_cerevisiae.R64-1-1.dna.toplevel.fa
-
-# Add Read Group information
+# ====== 添加Read Group ======
 gatk AddOrReplaceReadGroups \
-     -I ./data/SRR5251644.dedup.bam \
-     -O ./data/SRR5251644.final.bam \
-     --RGID SRR5251644 \
-     --RGSM SRR5251644 \
-     --RGPL ILLUMINA \
-     --RGLB lib1 \
-     --RGPU unit1
-
-gatk AddOrReplaceReadGroups \
-     -I ./data/SRR5251643.dedup.bam \
-     -O ./data/SRR5251643.final.bam \
+     -I $data_dir/SRR5251643.dedup.bam \
+     -O $data_dir/SRR5251643.final.bam \
      --RGID SRR5251643 \
      --RGSM SRR5251643 \
      --RGPL ILLUMINA \
      --RGLB lib1 \
      --RGPU unit1
 
-# Index the final bam files
-samtools index ./data/SRR5251644.final.bam
-samtools index ./data/SRR5251643.final.bam
+gatk AddOrReplaceReadGroups \
+     -I $data_dir/SRR5251644.dedup.bam \
+     -O $data_dir/SRR5251644.final.bam \
+     --RGID SRR5251644 \
+     --RGSM SRR5251644 \
+     --RGPL ILLUMINA \
+     --RGLB lib1 \
+     --RGPU unit1
 
-# Use GATK HaplotypeCaller to call variants
+# ====== 变异检测 ======
 gatk HaplotypeCaller \
-     -R ./ref/Saccharomyces_cerevisiae.R64-1-1.dna.toplevel.fa \
-     -I ./data/SRR5251644.final.bam \
-     -O ./vcf/SRR5251644.vcf
+     -R $ref_dir/Saccharomyces_cerevisiae.R64-1-1.dna.toplevel.fa \
+     -I $data_dir/SRR5251643.final.bam \
+     -O $vcf_dir/SRR5251643.g.vcf.gz \
+     -ERC GVCF \
+     --dont-use-soft-clipped-bases \
+     --native-pair-hmm-threads 4
 
 gatk HaplotypeCaller \
-     -R ./ref/Saccharomyces_cerevisiae.R64-1-1.dna.toplevel.fa \
-     -I ./data/SRR5251643.final.bam \
-     -O ./vcf/SRR5251643.vcf
+     -R $ref_dir/Saccharomyces_cerevisiae.R64-1-1.dna.toplevel.fa \
+     -I $data_dir/SRR5251644.final.bam \
+     -O $vcf_dir/SRR5251644.g.vcf.gz \
+     -ERC GVCF \
+     --dont-use-soft-clipped-bases \
+     --native-pair-hmm-threads 4
 
-# Filter the VCF files using GATK VariantFiltration
+# ====== 变异过滤 ======
 gatk VariantFiltration \
-     -V ./vcf/SRR5251644.vcf \
-     -O ./vcf/SRR5251644.filtered.vcf \
+     -V $vcf_dir/SRR5251643.g.vcf.gz \
+     -O $vcf_dir/SRR5251643.filtered.vcf \
      --filter-expression "QD < 2.0 || FS > 60.0 || MQ < 40.0" \
      --filter-name "filtration"
 
 gatk VariantFiltration \
-     -V ./vcf/SRR5251643.vcf \
-     -O ./vcf/SRR5251643.filtered.vcf \
+     -V $vcf_dir/SRR5251644.g.vcf.gz \
+     -O $vcf_dir/SRR5251644.filtered.vcf \
      --filter-expression "QD < 2.0 || FS > 60.0 || MQ < 40.0" \
      --filter-name "filtration"
 
-# Install SnpEff for annotation
-wget https://snpeff.blob.core.windows.net/versions/snpEff_latest_core.zip
-unzip snpEff_latest_core.zip
-
-# Find and download the SnpEff database for Saccharomyces cerevisiae
-java -jar snpEff/snpEff.jar databases | grep Saccharomyces_cerevisiae
-java -jar snpEff/snpEff.jar download Saccharomyces_cerevisiae
-
-# Run the annotation using SnpEff and generate statistical reports
+# ====== 变异注释 ======
 java -Xmx8g -jar snpEff/snpEff.jar Saccharomyces_cerevisiae \
-     ./vcf/SRR5251644.filtered.vcf > ./vcf/SRR5251644.annotated.vcf -stats ./vcf/SRR5251644_snpEff_summary.html
+     $vcf_dir/SRR5251643.filtered.vcf > $vcf_dir/SRR5251643.annotated.vcf \
+     -stats $vcf_dir/SRR5251643_snpEff_summary.html
 
 java -Xmx8g -jar snpEff/snpEff.jar Saccharomyces_cerevisiae \
-     ./vcf/SRR5251643.filtered.vcf > ./vcf/SRR5251643.annotated.vcf -stats ./vcf/SRR5251643_snpEff_summary.html 
+     $vcf_dir/SRR5251644.filtered.vcf > $vcf_dir/SRR5251644.annotated.vcf \
+     -stats $vcf_dir/SRR5251644_snpEff_summary.html
